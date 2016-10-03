01d9b5fab93f863d17363bb82b6d036c3b4eb4e0
Vitaly Bolychev <vb@devim.team>
2016-10-03 17:00:11 +0300

    Return JsonbArrayType class

---
+++ src/Type/JsonbArrayType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Devim\Provider\DoctrineExtendServiceProvider\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\JsonArrayType;
+
+/**
+ * Array Type which can be used to generate jsonb arrays. It uses the Doctrine JsonArrayType.
+ *
+ * @author Robin Boldt <boldtrn@gmail.com>
+ */
+class JsonbArrayType extends JsonArrayType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'JSONB';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jsonb';
+    }
+}
